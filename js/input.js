c75def594a42107c1baf0d86666a769fbbfa162b
tarheel300 <andypguice@gmail.com>
2023-08-21 23:39:44 -0400

    Adding HTML Elements and Function Callback

--- js/input.js
+++ js/input.js
@@ -1,19 +1,29 @@
+function acctBalanceSnshSubmit() {
+    console.log('Button Clicked!')
+}
+
+function initialize() {
+    //Add Event Listeners
+    var acctBalanceSnshBtn = document.getElementById('acctBalanceSnshBtn');
+
+    acctBalanceSnshBtn.addEventListener('click', acctBalanceSnshSubmit)
+}
+
 function addAccountSelectOptions() {
     getAccounts()
         .then(acctData => {
             console.log(acctData)
             
-            const acctDropDown = d3.select('acctSelect')
-
+            const acctDropDown = d3.select('#acctSelect')
 
             acctDropDown.selectAll('option')
                 .data(acctData)
                 .enter()
-                    .text(d => d.acct_nm)
-                    .attr('value', 'testing')
-                    .append('option');
+                .append('option')
+                .text(d => `${d.acct_nm} (${d.acct_nbr}) at ${d.inst_nm}`)
+                .attr('value', d => `${d.acct_id}`);
         });
 }
 
-
+document.addEventListener('DOMContentLoaded', initialize);
 addAccountSelectOptions();
